83977b56171276e1d6dcc5a85591fa693a437ed9
BeatrizFOliveira <89812265+BeatrizFOliveira@users.noreply.github.com>
2025-12-21 21:14:13 -0300

    Aula 001 e projeto 001

---
+++ aula001-Basico.py
@@ -0,0 +1,26 @@
+#Isso é um comentário
+
+"""
+Isso 
+também
+é
+um
+comentário
+"""
+
+#Como printar uma mensagem no console
+
+print('Hello world!')
+print('This' + 'is' + 'a' + 'concatenation')
+
+#Como inserir informações no console
+
+input('Qual o seu nome? ')
+print('Hello' + input('Qual o seu nome? '))
+
+#Variáveis
+
+nome = input('Qual o seu nome? ')
+print(nome)
+print(len(nome)) #A função len() conta a quantidade de caracteres de uma string
+
---
+++ aula001-Projeto.py
@@ -0,0 +1,7 @@
+print('Bem-vindo ao gerador de username!')
+
+nome = input('Digite o seu apelido: ')
+caract = input('Escreva um adjetivo que te define: ')
+dia = input('Digite o dia do seu nascimento: ')
+
+print(nome + '_' + caract + dia)
